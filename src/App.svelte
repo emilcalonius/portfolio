3de7ed95a55cb1c1c325ee7499347811b5f7e5e6
Emil Calonius <emil.calonius@tuni.fi>
2023-04-11 02:25:54 +0300

    clean up code

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,65 @@
 <script lang="ts">
   import Introduction from "./lib/Introduction.svelte";
   import Projects from "./lib/Projects.svelte";
-  import { fade } from 'svelte/transition';
-  import { onMount } from 'svelte';
+  import { fade } from "svelte/transition";
 
-  let hasScrolled = false;
-  window.onscroll = function (e: WheelEvent) {
-    if(window.pageYOffset > 0) {
-      hasScrolled = true;
-    } else {
-      hasScrolled = false;
-    }
-  }
+  let y: number;
 
   const scrollToTop = () => {
     window.scrollTo(0, 0);
-    setTimeout(() => {
-      showArrow();
-    }, 900);
-  }
-
-  const showArrow = () => {
-    const introSection = document.querySelector(".introduction");
-    if(window.innerHeight >= 865) {
-      document.querySelector(".arrow").style.display = "block";
-    }
-  }
-
-  onMount(() => {
-    showArrow();
-  });
+  };
 </script>
 
 <Introduction />
 <Projects />
-{#if !hasScrolled}
-<div class="arrow" out:fade></div>
-{:else}
-<button on:click={scrollToTop} class="toTop" out:fade in:fade>Top</button>
+{#if y == 0}
+  <div class="arrow" out:fade in:fade />
+{/if}
+{#if y > 0}
+  <button on:click={scrollToTop} class="toTop" out:fade in:fade>Top</button>
 {/if}
 
+<svelte:window bind:scrollY={y} />
+
 <style scoped>
-.toTop {
-  position: fixed;
-  bottom: 2rem;
-  right: 2rem;
-  padding: 1rem;
-  background-color: var(--ec-pink);
-  border-radius: 5px;
-  cursor: pointer;
-  transition: all 0.5s ease 0s;
-  border: 0;
-  z-index: 2;
-}
+  .toTop {
+    position: fixed;
+    bottom: 2rem;
+    right: 2rem;
+    padding: 1rem;
+    background-color: var(--ec-pink);
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.5s ease 0s;
+    border: 0;
+    z-index: 2;
+  }
 
-.toTop:hover {
-  background-color: hsl(2, 58%, 68%);;
-}
+  .toTop:hover {
+    background-color: hsl(2, 58%, 68%);
+  }
 
-.arrow {
-  width: 0; 
-  height: 0; 
-  border-left: 40px solid transparent;
-  border-right: 40px solid transparent;
-  border-top: 40px solid var(--ec-green-dark);
-  animation: bounce 2s linear infinite;
-  position: fixed;
-  bottom: 2rem;
-  left: 50%;
-  margin-left: -30px;
-}
+  .arrow {
+    width: 0;
+    height: 0;
+    border-left: 40px solid transparent;
+    border-right: 40px solid transparent;
+    border-top: 40px solid var(--ec-green-dark);
+    animation: bounce 2s linear infinite;
+    position: fixed;
+    bottom: 2rem;
+    left: 50%;
+    margin-left: -30px;
+  }
 
-@keyframes bounce {
-    0%, 100% {
-        transform: translateY(30%);
+  @keyframes bounce {
+    0%,
+    100% {
+      transform: translateY(30%);
     }
 
     50% {
-        transform: translateY(60%);
+      transform: translateY(60%);
     }
-}
-
-.arrow {
-    display: none;
-}
+  }
 </style>
--- src/lib/Introduction.svelte
+++ src/lib/Introduction.svelte
@@ -1,89 +1,93 @@
 <script lang="ts">
-import portrait from "../assets/portrait.jpg";
-import github from "../assets/github.png";
-import gitlab from "../assets/gitlab.png";
-import linkedin from "../assets/linkedin.png";
+  import portrait from "../assets/portrait.jpg";
+  import github from "../assets/github.png";
+  import gitlab from "../assets/gitlab.png";
+  import linkedin from "../assets/linkedin.png";
 </script>
 
 <div class="introduction">
-    <div class="profile">
-        <div class="portrait-border">
-            <div class="portrait">
-                <img class="portrait-img" src={portrait} alt="portrait">
-            </div>
-        </div>
-        <div class="text-container">
-            <div class="title">
-                <h1 class="title-text">Hi, </h1>
-                <h1 class="title-text">I'm </h1>
-                <h1 class="name">Emil Calonius</h1>
-                <h1 class="title-text"> | </h1>
-                <h1 class="name">Full Stack</h1>
-                <h1 class="title-text"> developer</h1>
-            </div>
-            <div class="links">
-                <a 
-                    href="https://github.com/emilcalonius" 
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    <img class="link" src={github} alt="GitHub">
-                </a>
-                <a 
-                    href="https://gitlab.com/emilcalonius" 
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    <img class="link" src={gitlab} alt="GitLab">
-                </a>
-                <a 
-                    href="https://www.linkedin.com/in/emilcalonius/" 
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    <img class="link" src={linkedin} alt="LinkedIn">
-                </a>
-            </div>
-            <p class="bio">
-                I am an aspiring web developer, who loves
-                problem solving and seeing the work I do
-                affect peoples lives positively. Making modern, responsive 
-                and practical web applications is my passion! 🔥
-            </p>
-            <p class="bio">
-                I like to keep up with new technologies and constantly improving 
-                my skills keeps me motivated. 
-                Take a look at my experience and projects below. ⬇️
-            </p>
-        </div>
+  <div class="profile">
+    <div class="portrait-border">
+      <div class="portrait">
+        <img class="portrait-img" src={portrait} alt="portrait" />
+      </div>
     </div>
+    <div class="text-container">
+      <div class="title">
+        <h1 class="title-text">Hi,</h1>
+        <h1 class="title-text">I'm</h1>
+        <h1 class="name">Emil Calonius</h1>
+        <h1 class="title-text">|</h1>
+        <h1 class="name">Full Stack</h1>
+        <h1 class="title-text">developer</h1>
+      </div>
+      <div class="links">
+        <a
+          href="https://github.com/emilcalonius"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img class="link" src={github} alt="GitHub" />
+        </a>
+        <a
+          href="https://gitlab.com/emilcalonius"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img class="link" src={gitlab} alt="GitLab" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emilcalonius/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img class="link" src={linkedin} alt="LinkedIn" />
+        </a>
+      </div>
+      <p class="bio">
+        I am an aspiring web developer, who loves problem solving and seeing the
+        work I do affect peoples lives positively. Making modern, responsive and
+        practical web applications is my passion! 🔥
+      </p>
+      <p class="bio">
+        I like to keep up with new technologies and constantly improving my
+        skills keeps me motivated. Take a look at my experience and projects
+        below. ⬇️
+      </p>
+    </div>
+  </div>
 </div>
 
 <style scoped>
-.title {
+  .title {
     display: flex;
     column-gap: 1rem;
     flex-wrap: wrap;
     cursor: default;
-}
+  }
 
-.title-text {
+  .title-text {
     display: flex;
     height: 5rem;
     font-size: 3rem;
     font-family: Inter;
     align-items: center;
     margin: 0;
-}
+  }
 
-.name {
+  .name {
     display: flex;
     height: 5rem;
     font-size: 4rem;
     font-family: Inter;
     align-items: center;
-    background-image: linear-gradient(90deg, 
-        var(--ec-pink), var(--ec-green-dark), var(--ec-pink), var(--ec-green-dark));
+    background-image: linear-gradient(
+      90deg,
+      var(--ec-pink),
+      var(--ec-green-dark),
+      var(--ec-pink),
+      var(--ec-green-dark)
+    );
     -webkit-background-clip: text;
     background-clip: text;
     color: transparent;
@@ -91,20 +95,19 @@ import linkedin from "../assets/linkedin.png";
     background-position: left;
     transition: background-position 1.5s;
     margin: 0;
-}
+  }
 
-.name:hover {
+  .name:hover {
     background-position: right;
-}
+  }
 
-.profile {
+  .profile {
     display: flex;
     gap: 2rem;
     flex-direction: column;
+  }
 
-}
-
-.introduction {
+  .introduction {
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -113,20 +116,20 @@ import linkedin from "../assets/linkedin.png";
     padding: 2rem;
     max-width: 1280px;
     margin: 0 auto;
-}
+  }
 
-.portrait-img {
+  .portrait-img {
     width: 20rem;
-}
+  }
 
-.portrait {
+  .portrait {
     overflow: hidden;
     width: 20rem;
     height: 20rem;
     border-radius: 50%;
-}
+  }
 
-.portrait-border {
+  .portrait-border {
     display: flex;
     place-items: center;
     justify-content: center;
@@ -134,103 +137,101 @@ import linkedin from "../assets/linkedin.png";
     min-height: 22rem;
     max-width: 22rem;
     max-height: 22rem;
-    background-image: linear-gradient(90deg, 
-        var(--ec-pink), var(--ec-green-dark), var(--ec-pink), var(--ec-green-dark));
+    background-image: linear-gradient(
+      90deg,
+      var(--ec-pink),
+      var(--ec-green-dark),
+      var(--ec-pink),
+      var(--ec-green-dark)
+    );
     border-radius: 50%;
     background-size: 300%;
     background-position: left;
     transition: background-position 1.5s;
-}
+  }
 
-.portrait-border:hover {
+  .portrait-border:hover {
     background-position: right;
-}
+  }
 
-.bio {
+  .bio {
     font-size: large;
     text-align: left;
     font-weight: lighter;
     font-family: sans-serif;
-}
+  }
 
-.text-container {
+  .text-container {
     max-width: 35rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
-}
+  }
 
-p {
+  p {
     margin: 0;
-}
+  }
 
-.links {
+  .links {
     display: flex;
     gap: 1rem;
-}
+  }
 
-.link {
+  .link {
     width: 2.5rem;
-}
+  }
 
-@media screen and (min-width: 1280px) {
+  @media screen and (min-width: 1280px) {
     .profile {
-        flex-direction: row;
+      flex-direction: row;
     }
-}
+  }
 
-@media screen and (max-width: 500px) {
+  @media screen and (max-width: 500px) {
     .name {
-        font-size: 3rem;
+      font-size: 3rem;
     }
 
     .title-text {
-        font-size: 2rem;
+      font-size: 2rem;
     }
-}
+  }
 
-@media screen and (max-height: 1000px) {
-    .portrait-img,
-    .portrait,
-    .portrait-border
-    {
-        
-    }
-
+  @media screen and (max-height: 1000px) {
     .profile {
-        align-items: center;
+      align-items: center;
     }
 
     .portrait {
-        width: 12rem;
-        height: 12rem;
+      width: 12rem;
+      height: 12rem;
     }
 
     .portrait-border {
-        min-width: 13rem;
-        min-height: 13rem;
-        max-width: 13rem;
-        max-height: 13rem;
+      min-width: 13rem;
+      min-height: 13rem;
+      max-width: 13rem;
+      max-height: 13rem;
     }
 
     .portrait-img {
-        width: 12rem;
+      width: 12rem;
     }
 
     .name {
-        height: 3rem;
+      height: 3rem;
     }
 
     .title-text {
-        height: 3rem;
+      height: 3rem;
     }
 
     .title {
-        column-gap: 0.5rem;
+      column-gap: 0.5rem;
     }
 
     .introduction {
-        justify-content: flex-start;
+      justify-content: flex-start;
     }
-}
+  }
 </style>
