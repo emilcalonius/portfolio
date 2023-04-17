3b8de4e7e16b04addf47c9d0bb58f934d5164dba
Emil Calonius <emil.calonius@tuni.fi>
2023-04-18 01:45:54 +0300

    mobile style fix for contact secion

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -4,28 +4,35 @@
 </script>
 
 <div class="contact">
-  <h1 class="title">Contact me:</h1>
-  <div class="links">
-    <a
-      href="https://www.linkedin.com/in/emilcalonius/"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <img class="link" src={linkedin} alt="LinkedIn" />
-    </a>
-    <a href = "mailto: emil.calonius@gmail.com">
-      <img class="link" src={email} alt="Email" />
-    </a>
+  <div class="contact-wrapper">
+    <h1 class="title">Contact me:</h1>
+    <div class="links">
+      <a
+        href="https://www.linkedin.com/in/emilcalonius/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img class="link" src={linkedin} alt="LinkedIn" />
+      </a>
+      <a href = "mailto: emil.calonius@gmail.com">
+        <img class="link" src={email} alt="Email" />
+      </a>
+    </div>
   </div>
+  <div class="copyright">© 2023 Emil Calonius</div>
 </div>
 
 <style scoped>
   .contact {
     background: rgb(22, 22, 22);
     padding: 2rem;
+  }
+
+  .contact-wrapper {
     display: flex;
     gap: 2rem;
     align-items: center;
+    padding-bottom: 2rem;
   }
 
   .links {
@@ -35,5 +42,11 @@
 
   .link {
     width: 2.5rem;
+  }
+
+  @media screen and (max-width: 1000px) {
+    .contact-wrapper {
+      display: block;
+    }
   }
 </style>
